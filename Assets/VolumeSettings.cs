b39a47e5fd4b69b3c7db71cf7a9e7fe55bc5d157
SCOOTKAPTAIN <scooteco9@gmail.com>
2025-08-21 14:22:09 +0700

    Volume Control added

---
+++ Assets/VolumeSettings.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider MusicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    public void Start()
+    {
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            loadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = MusicSlider.value;
+        myMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    private void loadVolume()
+    {
+        MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+}
